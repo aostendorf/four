e840850184a058490ea899ddf9f9af61f59f4cec
Amber Ostendorf <ambersed_2000@yahoo.com>
2018-06-01 10:23:22 -0600

    initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+ruby 2.5.0
+
+gem 'to_words', '~> 1.1', '>= 1.1.1'
---
+++ four_is_magic_number.rb
@@ -0,0 +1,20 @@
+require 'to_words'
+
+def four
+  puts "Please input a number or type quit"
+  number = gets.strip.to_int 
+  word = number.to_words.gsub(" ", "").length
+  leters = []
+  letters<<words.split
+  count = letters.count
+
+  puts "number is #{count}"
+    if number = count
+      puts "and 4 is the magic number"
+    else
+
+
+end
+
+four
+
